6dbfd83cf8a82c093a08fcfff926ee07964402bd
bellDev-code <sumaoo20@naver.com>
2023-06-19 22:22:56 +0900

    setting & react-router-dom v6 setting

---
+++ src/Layouts/Main/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomePage from "../../Pages/Home";
+
+const Main = () => {
+    return <HomePage />
+}
+
+export default Main;
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>홈 라우터</div>
+    )
+}
+
+export default Home
