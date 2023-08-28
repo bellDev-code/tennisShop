7de0288385233f1a26d64b78061bf92f5a9ca92f
bellDev-code <sumaoo20@naver.com>
2023-08-28 17:59:06 +0900

    Add mockup data

---
+++ src/Components/Category/mockup.tsx
@@ -0,0 +1,75 @@
+import saleIcon from "../../assets/Category/icon_sale.png"
+import newIcon from "../../assets/Category/icon_new.png"
+import racketIcon from "../../assets/Category/icon_racket.png"
+import dampIcon from "../../assets/Category/icon_damper.png"
+import maleIcon from "../../assets/Category/icon_shoes.png"
+import femaleIcon from "../../assets/Category/icon_women.png"
+import backIcon from "../../assets/Category/icon_bag.png"
+import ballIcon from "../../assets/Category/icon_ball.png"
+import sobiIcon from "../../assets/Category/icon_con.png"
+//
+import stringIcon from "../../assets/Category/icon_repair.png"
+import reserveIcon from "../../assets/Category/icon_calendar.png"
+
+// 임시 데이터
+
+export const serviceList = [
+    {
+        title: '스트링 수리',
+        url: '',
+        image: stringIcon
+    },
+    {
+        title: '리저브(예약)',
+        url: '',
+        image: reserveIcon
+    }
+]
+
+export const categoryList = [
+    {
+        title: '세일',
+        url: '/sale',
+        image: saleIcon
+    },
+    {
+        title: '신상',
+        url: '/sale',
+        image: newIcon
+    },
+    {
+        title: '라켓',
+        url: '/sale',
+        image: racketIcon
+    },
+    {
+        title: '댐프너',
+        url: '/sale',
+        image: dampIcon
+    },
+    {
+        title: '남성',
+        url: '/sale',
+        image: maleIcon
+    },
+    {
+        title: '여성',
+        url: '/sale',
+        image: femaleIcon
+    },
+    {
+        title: '가방',
+        url: '/sale',
+        image: backIcon
+    },
+    {
+        title: '테니스볼',
+        url: '/sale',
+        image: ballIcon
+    },
+    {
+        title: '소비품',
+        url: '/sale',
+        image: sobiIcon
+    },
+]
