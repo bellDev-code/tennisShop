f525a818d6903758621a45c4fce5cf218586bf69
bellDev-code <sumaoo20@naver.com>
2023-08-29 09:50:55 +0900

    code refactoring

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -2,37 +2,33 @@ import { categoryList, serviceList } from "./mockup"
 import { CategoryBox, CategoryFont, CategoryTitle, Container, ContentsBox, ContentsFont, ContentsWrap, ServiceBox } from "./styles"
 
 
+interface Item {
+    title: string;
+    image: string;
+}
+
 const Category = () => {
+    const renderContents = (list: Item[]) => {
+        return list.map((item) => (
+          <ContentsWrap key={item.title}>
+            <img src={item.image} alt="icon" />
+            <ContentsFont>{item.title}</ContentsFont>
+          </ContentsWrap>
+        ));
+      };
+    
     return (
         <Container>
-            <CategoryTitle>+ 3,000여 개의 테니스 제품이 준비되어 있어요.</CategoryTitle>
-            <CategoryBox>
-            <CategoryFont>카테고리</CategoryFont>
-                <ContentsBox>
-                {categoryList.map((i) => {
-                    return (
-                            <ContentsWrap>
-                                <img src={i.image} alt="icon" />
-                                <ContentsFont>{i.title}</ContentsFont>
-                            </ContentsWrap>
-                        )
-                    })}
-                </ContentsBox>
-            </CategoryBox>
-            <ServiceBox>
-            <CategoryFont>서비스</CategoryFont>
-                <ContentsBox>
-                {serviceList.map((i) => {
-                    return (
-                            <ContentsWrap>
-                                <img src={i.image} alt="icon" />
-                                <ContentsFont>{i.title}</ContentsFont>
-                            </ContentsWrap>
-                        )
-                    })}
-                </ContentsBox>
-                </ServiceBox>
-        </Container>
+      <CategoryTitle>+ 3,000여 개의 테니스 제품이 준비되어 있어요.</CategoryTitle>
+      <CategoryBox>
+        <CategoryFont>카테고리</CategoryFont>
+        <ContentsBox>{renderContents(categoryList)}</ContentsBox>
+      </CategoryBox>
+      <ServiceBox>
+        <CategoryFont>서비스</CategoryFont>
+        <ContentsBox>{renderContents(serviceList)}</ContentsBox>
+      </ServiceBox>
+    </Container>
     )
 }
 
